0bb3a8808e884eb1cc76e0c2304cafd19764ea2d
454 <g920430@gmail.com>
2024-06-12 09:13:32 +0800

    新增手持裝備的Prefab & 所有模型增加Handheld & Handheld_Equipment寫一半

---
+++ Assets/Script/Animation/Handheld_Equipment.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+
+
+public class Handheld_Equipment : MonoBehaviour
+{
+    public GameObject[] equimentPrefab = new GameObject[3];
+    public Player player;
+    public GameObject handheld_Transform; //模型上的手持位置
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Handheld(int equimentNum)
+    {
+        GameObject equiment = Instantiate(equimentPrefab[equimentNum - 1]);
+        ParentConstraint parentConstraint = equiment.GetComponent<ParentConstraint>();
+
+        //將Gameobject轉化為ConstraintSource單位
+        ConstraintSource constraintSource = new ConstraintSource 
+        {
+            sourceTransform = equimentPrefab[equimentNum - 1].transform,
+            weight = 1.0f
+        };
+
+        parentConstraint.AddSource(constraintSource);
+    }
+
+    public void EquipmentJudge() //判斷武器
+    {
+        switch (player._equipment)
+        {
+            case Player_Controller.Equipment.drill://鑽頭
+                Handheld(1);
+                break;
+            case Player_Controller.Equipment.pile_driver://打樁機
+                Handheld(2);
+                break;
+            case Player_Controller.Equipment.demolition_hamer://鑿破機
+                Handheld(3);
+                break;
+            default://以上都不符合走這個
+                Debug.LogError("Handheld_Equment 的 EquipmentJude 出錯 ");
+                break;
+        }
+    }
+}
